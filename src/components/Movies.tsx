78b1ecfed6f3195906865b8e554802fce7b604a6
soumyaDasgupta34 <soumyadasgupta34@gmail.com>
2022-05-27 01:30:41 +0530

    First commit

---
+++ src/components/Movies.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Movies = (props: any) => {
+  console.log("Inside movies", props.movie);
+  return (
+    <div className="flex flex-col justify-between mb-8 flex-1">
+      <img
+        src={props.movie.image}
+        alt=""
+        className="h-9/12 w-9/12 rounded-md cursor-pointer"
+      />
+      <div className="text-lg font-bold">{props.movie.name}</div>
+      <div className="text-sm text-gray-500">{props.movie.rating}</div>
+      <div className="text-sm text-gray-500">{props.movie.language}</div>
+    </div>
+  );
+};
+
+export default Movies;
